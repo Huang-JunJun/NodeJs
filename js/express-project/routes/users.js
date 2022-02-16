6e7e0ef730a28ce98e4b3ccd4e1bf2eebfcd7d8b
黄显竣 <770894595@qq.com>
2022-02-17 01:29:25 +0800

    连接数据库代码和注册页面编写

--- js/express-project/routes/users.js
+++ js/express-project/routes/users.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+// 数据库集合的相关配置
+// 1. 定义数据集合的结构
+// 定义出集合重的数据有哪些属性 属性的值是哪些类型
+const { Schema, model } = require('mongoose')
+const usersSchema = new Schema({
+  username: String,
+  password: String
+})
+
+// 2.定义数据集合的模型： 将schema和数据库中的集合关联起来
+// model('模型名称', Schema的名称, '数据库中的集合名称')
+model('userModel', usersSchema, 'account')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -11,6 +24,8 @@ router.post('/login', function(req, res, next) {
   // - post： req.body
   // - get: req.query
   // send方法处理响应结果给到前端
+  // 接收到前端发送的用户数据
+  const user = req.body
   res.send({
     msg: '登录成功',
     status: 200
